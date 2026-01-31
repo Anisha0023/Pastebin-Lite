69a81a08b1333185a3d9b20b457fe03f3a714eec
Anisha0023 <anishaanishajk@gmail.com>
2026-01-31 16:36:11 +0530

    fix

--- backend/src/utils/cors.js
+++ backend/src/utils/cors.js
@@ -1,5 +1,5 @@
 export function setCors(res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://pastebin-lite-seven-sepia.vercel.app/"); 
+  res.setHeader("Access-Control-Allow-Origin", "https://pastebin-lite-seven-sepia.vercel.app"); 
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 }
